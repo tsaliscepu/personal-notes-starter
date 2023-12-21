181c8f493b9dc0d41c3bfc2e8806599cca7bf21d
tsaliscepu <tsaliscepu@gmail.com>
2023-12-21 15:46:24 +0700

    Initial Commit, start working

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,7 @@ import { createRoot } from 'react-dom/client';
 // import style
 import './styles/style.css';
 
+const element = <h1>Hello, world!</h1>;
+
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, World!</h1>);
+root.render(element);
